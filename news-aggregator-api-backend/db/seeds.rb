49aae179f51a060c3154c30c7b89ccaf4543de9f
boom100100 <bsd231@nyu.edu>
2020-02-12 17:01:13 -0500

    Added migrations and seeds.

--- news-aggregator-api-backend/db/seeds.rb
+++ news-aggregator-api-backend/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(name: 'Luke', password: "password1")
+User.create(name: 'Luke', password: "password1")
+User.create(name: 'Luke', password: "password1")
+User.create(name: 'Luke', password: "password1")
+User.create(name: 'Luke', password: "password1")
+User.create(name: 'Luke', password: "password1")
+
+Topic.create(name: '#politics')
+Topic.create(name: '#culture')
+
+Link.create(name: 'Black Panther', url: "https://google.com")
+Link.create(name: 'Iowa', url: "https://google.com")
