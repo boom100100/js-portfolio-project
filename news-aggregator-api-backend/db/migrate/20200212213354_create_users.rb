c93585e8eda9030c113f8d290b056e605b1a9899
boom100100 <bsd231@nyu.edu>
2020-02-12 17:38:16 -0500

    Modified user migration.

--- news-aggregator-api-backend/db/migrate/20200212213354_create_users.rb
+++ news-aggregator-api-backend/db/migrate/20200212213354_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :username
       t.string :password
+      t.string :location, :default => ''
 
       t.timestamps
     end
