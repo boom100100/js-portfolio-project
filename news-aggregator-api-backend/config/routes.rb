d9626649f58a6e2187eae61a348032e82b14824b
boom100100 <bsd231@nyu.edu>
2020-02-13 21:41:47 -0500

    Added routes template.

--- news-aggregator-api-backend/config/routes.rb
+++ news-aggregator-api-backend/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :links
   resources :topics
   resources :users
+  #  resources :articles, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
