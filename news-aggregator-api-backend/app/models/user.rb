ab733cae002b7db4bff73b660406199670e73fb7
boom100100 <bsd231@nyu.edu>
2020-02-13 20:20:23 -0500

    Added secure password functionality .

--- news-aggregator-api-backend/app/models/user.rb
+++ news-aggregator-api-backend/app/models/user.rb
@@ -1,4 +1,12 @@
+=begin
+class User < ActiveRecord::Base
+  has_secure_password
+end
+=end
+
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :links
   has_many :topics, through :links
 end
