d1d9e6c4b7bc2e62fd136652454b35d7697b3150
boom100100 <bsd231@nyu.edu>
2020-02-15 18:50:27 -0500

    Added functionality for getting Twitter API data, rendering derived objects.

--- news-aggregator-api-backend/app/controllers/topics_controller.rb
+++ news-aggregator-api-backend/app/controllers/topics_controller.rb
@@ -1,4 +1,50 @@
 class TopicsController < ApplicationController
-  def show
+
+  def index
+    #authenticate
+    client = authenticate
+
+    #gets trends
+    topics = client.trends().to_h
+    if topics.as_json['trends'].length > 0
+      destroy #gets rid of previous topics
+      topics.as_json['trends'].each do |topic|
+        create(topic['name']) #creates topic objects
+        #puts topic['name']
+      end
+      render json: Topic.all, only: ['id','name']
+    else
+      render 'Collecting trending topics failed.'
+    end
   end
+
+  def show
+    topic = Topic.find_by(id: params[:id])
+    if topic
+      render json: topic, only: ['name']
+    else
+      render plain: 'Topic not found.'
+    end
+  end
+
+  def new
+    Topic.new
+  end
+
+  def create(name)
+    Topic.create(name: name).save
+  end
+
+  def destroy
+    Topic.delete_all
+  end
+
+  private
+  def authenticate
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = ""
+      config.consumer_secret     = ""
+    end
+  end
+
 end
