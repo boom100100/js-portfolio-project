db3840b09cd1768d68ef87a96c385036ceb3b421
boom100100 <bsd231@nyu.edu>
2020-02-15 21:30:25 -0500

    Updated routes.

--- news-aggregator-api-backend/config/routes.rb
+++ news-aggregator-api-backend/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :links
-  resources :topics
-  resources :users
+  #resources :links
+  #root 'application#index'
+  #route 'welcome#index'
+  resources :topics, only: [:index, :show]#routes not required: :new, :create, :destroy; completely unutilized: :edit, :update
+  resources :links, only: [:index, :show]#routes not required: :new, :create, :destroy; completely unutilized: :edit, :update
+  #resources :users
   #  resources :articles, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
