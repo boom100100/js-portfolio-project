27cf2c7cc62abd5936be3ebdf058e6654f41527e
boom100100 <bsd231@nyu.edu>
2020-02-16 18:16:07 -0500

    Fixed backend api call.

--- news-aggregator-api-backend/app/controllers/links_controller.rb
+++ news-aggregator-api-backend/app/controllers/links_controller.rb
@@ -1,36 +1,48 @@
 require 'news-api'
+require "open-uri"
 
 class LinksController < ApplicationController
+  #@@time;
   def index
-    #authenticate news
-    news = News.new("")
+
 
     #get topics, date
     topics = Topic.all
     date = format
 
+
+    #if links updated within the hour: if (@@time - date[1]).abs != 0
+    #if there are trending topics
     if topics
+      #@@time = format[1]
       destroy
+
       #search each topic
       topics.each do |topic|
-        name = topic.name
-        news.get_everything(q: "monopoly", from: "#{date}", sortBy: "popularity").as_json(only: ['name', 'title', 'url']).each do |result|
+        name = topic.name.gsub(/[\W\s]/, "")
+
+        #authenticate news
+        articles = getNews(topic.name, date[0])
+
+        articles["response"]["results"].each do |result|
+        #results = news.get_everything(q: "#{name}", from: "#{date[0]}", sortBy: "popularity")
+
+        #results.each do |result|
 
           #make link for each search result
-          Link.create(name: result['title'], url: result['url'], topic: topic).save
+          Link.create(name: result['webTitle'], url: result['webUrl'], topic: topic).save
         end
       end
 
-      render json: Link.all, only: [:id, :name, :url, :topic]
+      render json: Link.all, only: [:id, :name, :url], include: [:topic]
     else
       render ''
     end
-    #render json: news.get_everything(q: "news", from: "#{date}", sortBy: "popularity").as_json(only: ['name', 'title', 'url'])
-    #render json: news.get_everything(q: "news", from: "#{date}", sortBy: "popularity")
+  end
 
-    #news.get_everything(q: "#{topic}", from: "#{date}", sortBy: "popularity")
-    #render json: news.get_sources(country: 'us', language: 'en'), only: ['id','name', 'url']
-    #render json: Link.all, only: ['id','name', 'url']
+  def show
+    link = Link.find_by(:id => params[:id])
+    render json: link, only: [:id, :name, :url], include: [:topic]
   end
 
   def new
@@ -48,10 +60,22 @@ class LinksController < ApplicationController
   private
   def format
     time = Time.now
-    year = time.year
-    month = time.month
+    year = time.strftime("%Y")
+    month = time.strftime("%m")
     day = time.day
-    formatted_string = "#{year}-#{month}-#{day-2}&to=#{year}-#{month}-#{day}"
-    formatted_string
+    hour = time.hour
+    formatted_strings = ["#{year}-#{month}-#{day}", "#{hour}"]#{hour}"] #&to=#{year}-#{month}-#{day}", "#{hour}"]
+    formatted_strings
+  end
+
+  def getNews(topic, date)
+    url = "https://content.guardianapis.com/search?keyword=#{topic.gsub(/[\s\W]/,'_')}&api-key="
+    #url = "https://content.guardianapis.com/search&keyword=#{topic.gsub(/[\s\W]/,'_')}?api-key=7d9a78bd-df7c-46cb-8317-ec287094e5fa"
+    #url = "https://api-beta.civicfeed.com/news/search?q=#{topic.gsub(/[\s\W]/,'_')}&from=#{date[0]}&results=30?x-api-key=2E1osqNeA413TDVuOOjU06kXSjgBy5qxPKnbhMt2"
+    #url = "https://api.currentsapi.services/v1/search?keywords=#{topic.gsub(/[\s\W]/,'_')}&language=en&apiKey=CfQ_cXFgfCVeOLgwoPUAVEbX4-vdtrrrjp8oA-KqwCOf5OAH"
+    #url = "https://newsapi.org/v2/everything?q=#{topic.gsub(/[\s\W]/,'_').downcase}?domains=aljazeera.com,arstechnica.com,arynews.tv,apnews.com,afr.com,axios.com,bbc.co.uk,bloomberg.com,businessinsider.com,cbc.ca,cbsnews.com,cnbc.com,cnn.com,elmundo.es,engadget.com,fortune.com,fourfourtwo.com,medicalnewstoday.com,msnbc.com,mtv.com,nationalgeographic.com,nbcnews.com,news24.com,newscientist.com,newsweek.com,nymag.com,nextbigfuture.com,nfl.com,nhl.com,politico.com,recode.net,reuters.com,rte.ie,wsj.com,nytimes.com&from=#{date}&apiKey=a025658f693f40f99ccd792a8392f074"
+    article_serialized = open(url).read
+    articles = JSON.parse(article_serialized)
+    articles
   end
 end
