37cdd271364708980d27ac51b80cec736014223d
boom100100 <bsd231@nyu.edu>
2020-02-15 21:30:56 -0500

    Edited seeds.

--- news-aggregator-api-backend/db/seeds.rb
+++ news-aggregator-api-backend/db/seeds.rb
@@ -7,12 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-User.create(name: 'Cliff', password: "password1", location: '')
-User.create(name: 'Tiesha', password: "password1", location: '')
-User.create(name: 'Abdul', password: "password1", location: '')
+#User.create(name: 'Cliff', password: "password1", location: '')
+#User.create(name: 'Tiesha', password: "password1", location: '')
+#User.create(name: 'Abdul', password: "password1", location: '')
 
-Topic.create(name: '#politics')
+@topic = Topic.create(name: '#politics')
 Topic.create(name: '#culture')
 
-Link.create(name: 'Black Panther', url: "https://google.com")
-Link.create(name: 'Iowa', url: "https://google.com")
+Link.create(name: 'Black Panther', url: "https://google.com", topic: @topic)
+Link.create(name: 'Iowa', url: "https://google.com", topic: @topic)
