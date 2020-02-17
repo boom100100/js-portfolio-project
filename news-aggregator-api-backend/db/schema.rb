e9fa0bec93e77e47d25db5caaad26afaca8dde1f
boom100100 <bsd231@nyu.edu>
2020-02-17 13:21:18 -0500

    Edited schema.

--- news-aggregator-api-backend/db/schema.rb
+++ news-aggregator-api-backend/db/schema.rb
@@ -15,12 +15,8 @@ ActiveRecord::Schema.define(version: 2020_02_12_213555) do
   create_table "links", force: :cascade do |t|
     t.string "name"
     t.string "url"
-    t.integer "topic_id"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["topic_id"], name: "index_links_on_topic_id"
-    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "topics", force: :cascade do |t|
