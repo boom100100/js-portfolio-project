54e8eacfa6167e39fba4fdda68b6e963d4b98bb0
boom100100 <bsd231@nyu.edu>
2020-02-16 23:12:51 -0500

    Edited links migration to remove relationship.

--- news-aggregator-api-backend/db/migrate/20200212213555_create_links.rb
+++ news-aggregator-api-backend/db/migrate/20200212213555_create_links.rb
@@ -3,8 +3,8 @@ class CreateLinks < ActiveRecord::Migration[5.2]
     create_table :links do |t|
       t.string :name
       t.string :url
-      t.belongs_to :topic
-      t.belongs_to :user
+      #t.belongs_to :topic
+      #t.belongs_to :user
 
       t.timestamps
     end
