a024d15d36d1c5cd9aca0f4dc18b70cd558aaf78
boom100100 <bsd231@nyu.edu>
2020-02-17 16:15:59 -0500

    Fixed relationship bug.

--- news-aggregator-api-backend/db/migrate/20200212213555_create_links.rb
+++ news-aggregator-api-backend/db/migrate/20200212213555_create_links.rb
@@ -3,7 +3,7 @@ class CreateLinks < ActiveRecord::Migration[5.2]
     create_table :links do |t|
       t.string :name
       t.string :url
-      #t.belongs_to :topic
+      t.belongs_to :topic
       #t.belongs_to :user
 
       t.timestamps
