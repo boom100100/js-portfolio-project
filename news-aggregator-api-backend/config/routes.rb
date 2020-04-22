b6abe8e6f9d0821ca51b9f7e64905706854c9177
boom100100 <bsd231@nyu.edu>
2020-04-22 18:59:41 -0400

    Added topics/refresh route. User can initiate updating topics to current data.

--- news-aggregator-api-backend/config/routes.rb
+++ news-aggregator-api-backend/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   #resources :links
   #root 'application#index'
   #route 'welcome#index'
-  resources :topics, only: [:index, :show]#routes not required: :new, :create, :destroy; completely unutilized: :edit, :update
+
+  get 'topics/refresh', to: 'topics#refresh'
+  resources :topics, only: [:index, :show] #routes not required: :new, :create, :destroy; completely unutilized: :edit, :update
   resources :links, only: [:index, :show]#routes not required: :new, :create, :destroy; completely unutilized: :edit, :update
   #resources :users
   #  resources :articles, only: [:index, :show, :new, :create, :edit, :update, :destroy]
