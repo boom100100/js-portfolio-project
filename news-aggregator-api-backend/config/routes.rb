6ebd65b8dba8883c811e3f7acdd6ad81074414a3
boom100100 <bsd231@nyu.edu>
2020-04-23 10:09:04 -0400

    Cleaned up routes.

--- news-aggregator-api-backend/config/routes.rb
+++ news-aggregator-api-backend/config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  #resources :links
-  #root 'application#index'
-  #route 'welcome#index'
 
   get 'topics/refresh', to: 'topics#refresh'
   resources :topics, only: [:index, :show] #routes not required: :new, :create, :destroy; completely unutilized: :edit, :update
   resources :links, only: [:index, :show]#routes not required: :new, :create, :destroy; completely unutilized: :edit, :update
-  #resources :users
-  #  resources :articles, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
