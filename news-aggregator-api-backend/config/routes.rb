1e5b434f60cf342c0e621f9ff43798e1db06255a
boom100100 <bsd231@nyu.edu>
2020-04-23 14:22:29 -0400

    Added links/refresh route.

--- news-aggregator-api-backend/config/routes.rb
+++ news-aggregator-api-backend/config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   get 'topics/refresh', to: 'topics#refresh'
   resources :topics, only: [:index, :show] #routes not required: :new, :create, :destroy; completely unutilized: :edit, :update
+  post 'links/refresh', to: 'links#refresh'
   resources :links, only: [:index, :show]#routes not required: :new, :create, :destroy; completely unutilized: :edit, :update
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
